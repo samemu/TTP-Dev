d2307c27f4966c9355d08589322d2ae8a1db96a0
rkalvait <rkalvait@emich.edu>
2017-03-23 23:55:07 -0400

    Made each meter unit unique. pushed to dockerhub.

--- docker/host/listen.py
+++ docker/host/listen.py
@@ -1,14 +1,18 @@
 import socket
+import time
+
 UDP_IP = "172.18.1.1"
 UDP_PORT = 8088
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-logfile = open('./datalog.txt', 'w')
+logfile = open('./datalog.csv', 'w')
 
 while True:
 	data, addr = (sock.recvfrom(1024))
 	print (data)
 	print (addr[0])
-	logfile.write( addr[0] + " " + data + "\n")
+	timestamp = str(int(time.time()))
+	logfile.write(timestamp + ","+ addr[0] + "," + data + "\n")
+	logfile.flush()
--- docker/report_hourly.py
+++ docker/report_hourly.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 import socket
+from sys import argv
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dist_ip = '172.18.1.1'
@@ -9,8 +10,10 @@ dist_port = 8088
 dfile = open('./data.csv', 'r')
 data = np.loadtxt(dfile, delimiter=',')
 
+col = int(argv[1])
+
 for i in range(len(data)):
-	current = str(data[i])
+	current = str(data[i][col])
 	sock.sendto(current, (dist_ip, dist_port))
 
 	time.sleep(10)
