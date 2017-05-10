2b5769ae9d9b7ddda9d09303e384bbc8b234e98e
rkalvait <rkalvait@emich.edu>
2017-05-10 14:29:28 -0400

    Removed rabdu, added markzz. Only stubs for now - need fix

--- docker/net_test.sh
+++ docker/net_test.sh
@@ -4,12 +4,12 @@ ncols="$(head -1 data.csv | sed 's/[^,]//g' | wc -c)"
 idx=0
 
 echo $ncols
-docker run --net smartnet --ip 172.18.1.1 -d rkalvait/blr_host
+docker run --net smartnet --ip 172.18.1.1 -d markzz/blr_host
 
 until [ $idx -eq $ncols ]
 do
 	echo $idx
-	docker run --net smartnet -d rabdul/blr python report_hourly.py $idx
+	docker run --net smartnet -d markzz/blr python report_hourly.py $idx
 	idx=`expr $idx + 1`
 done
 
