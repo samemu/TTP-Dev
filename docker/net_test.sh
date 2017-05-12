94b4d4c84a1a62c0c78dd558834ecd6424f03ac8
rkalvait <rkalvait@emich.edu>
2017-05-12 18:10:15 -0400

    snall change for testing

--- docker/net_test.sh
+++ docker/net_test.sh
@@ -5,11 +5,13 @@ idx=0
 
 echo $ncols
 docker run --net smartnet --ip 172.18.1.1 -d markzz/blr_host
+#docker run --net smartnet --ip 172.18.1.1 -d rkalvait/blr_host #uncomment for testing
 
 until [ $idx -eq $ncols ]
 do
 	echo $idx
 	docker run --net smartnet -d markzz/blr python report_hourly.py $idx
+	#docker run --net smartnet -d rkalvait/blr python report_hourly.py $idx #uncomment for testing
 	idx=`expr $idx + 1`
 done
 
